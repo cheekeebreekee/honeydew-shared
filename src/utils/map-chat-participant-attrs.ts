85fb511bfca54072df1e0a8a818cf6d0ef42a51d
Anton Filipovich <anton_filipovich1@epam.com>
2023-02-16 00:10:37 +0100

    fix deployment issue

--- src/utils/map-chat-participant-attrs.ts
+++
@@ -1,6 +0,0 @@
-import { ParticipantInstance } from "twilio/lib/rest/conversations/v1/conversation/participant";
-import { ChatParticipantAttributes } from "../types/Chat";
-
-export const mapChatParticipantAttributes = (
-  participant: ParticipantInstance
-): ChatParticipantAttributes => JSON.parse(participant.attributes);
