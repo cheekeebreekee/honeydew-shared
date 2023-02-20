375fb5ea4cd371d2ddeefa28fb3c0c5667e2ae40
Anton Filipovich <anton_filipovich1@epam.com>
2023-02-20 22:55:47 +0100

    fix import

--- src/notifications/sms/care-coordinator/index.ts
+++ src/notifications/sms/care-coordinator/index.ts
@@ -1,3 +1,2 @@
 export { appointmentToDecline } from "./appointment-to-decline";
-export { newMessageInChat } from "./new-message-in-chat";
 export { newAppointment } from "./new-appointment";
--- src/notifications/sms/index.ts
+++ src/notifications/sms/index.ts
@@ -1,3 +1,4 @@
 export * as Provider from "./provider";
 export * as Patient from "./patient";
 export * as CareCoordinator from "./care-coordinator";
+export * from "./new-message-in-chat";
