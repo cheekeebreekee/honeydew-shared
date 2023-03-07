7cdb4568e1759a0faf75c881df0732b09de41f2d
Yahor Simak <yahorsimak@Yahors-iMac.gdansk-morena.vectranet.pl>
2023-03-07 19:01:12 +0100

    feat: update version

--- src/events/detail-types.ts
+++ src/events/detail-types.ts
@@ -2,4 +2,5 @@ export enum DETAIL_TYPES {
   NOTIFICATIONS = "Honeydew Email/SMS Notification Event",
   SMS_REPLIES = "Honeydew Users SMS Replies",
   CHAT_ACTIONS = "Honeydew Twilio Chat Actions",
+  APPOINTMENT_CREATED = "appointment/created"
 }
