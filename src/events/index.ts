835a19de9783970cacdb5bb377f42f1a469a69fe
Yahor Simak <yahorsimak@Yahors-iMac.gdansk-morena.vectranet.pl>
2023-03-07 21:28:59 +0100

    feat: adjust event bridge client

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,19 +1,13 @@
 import AWS_EventBridge, { EventBridge } from "@aws-sdk/client-eventbridge";
 import { DETAIL_TYPES } from "./detail-types";
 
-const eventBridge = new EventBridge({
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-    secretAccessKey: process.env.AWS_SECRET as string,
-  },
-  region: process.env.AWS_REGION,
-});
+const eventBridge = new EventBridge({});
 
 export const publishEvent = async (data: string, detailType: DETAIL_TYPES) => {
   const params: AWS_EventBridge.PutEventsCommandInput = {
     Entries: [
       {
-        Source: process.env.AWS_LAMBDA_FUNCTION_NAME,
+        Source: process.env.SERVICE,
         Detail: data,
         DetailType: detailType,
       },
