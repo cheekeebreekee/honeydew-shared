35d78b3e50b1e3dd4d61ecf8ccadf0237a7c8ba2
Yahor Simak <yahorsimak@Yahors-iMac.gdansk-morena.vectranet.pl>
2023-03-07 18:50:14 +0100

    feat: update get resource name function

--- src/utils/get-resource-name.ts
+++ src/utils/get-resource-name.ts
@@ -1,4 +1,6 @@
 export const getResourceName = (resourcePartialName: string): string => {
+  console.log("Env variable", process.env);
+
   const resourceName = `${resourcePartialName}-${process.env.ENVIRONMENT}`;
 
   return resourceName;
